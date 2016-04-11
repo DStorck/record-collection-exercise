cba20b0b4ac9a31b76b1a03307d647336284de71
Deirdre Storck <deirdre.storck@gmail.com>
2016-04-11 16:52:51 -0700

    wrote load! method and got it albums loaded into the database

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
+require 'csv'
 
 class ReleaseDatabase
+  FILE_PATH = 'source/20160406-record-collection.csv'
   attr_reader :db
 
   def initialize(dbname = "releases")
@@ -9,7 +11,7 @@ class ReleaseDatabase
 
   def reset_schema!
     #table name: albums
-    #label_code,artist,title,label,format,year released,date_added
+    #label_code, :artist,title,label,format,released,date_added
     #blob, text, text, text, text, integer, text
     #keep in one table
     query = <<-CREATESTATEMENT
@@ -24,13 +26,35 @@ class ReleaseDatabase
     date_added TEXT
     );
     CREATESTATEMENT
-    @db.execute('DROP TABLE IF EXISTS albums;') #this will reset table if table already exists
 
+    @db.execute('DROP TABLE IF EXISTS albums;') #this will reset table if table already exists
     @db.execute(query) #execute can only run one query at a time
+  end
+
+  def load!
+    #statement preparation designed for reuse, it is a query , will work sort of like interpolation
+    insert_statement = <<-INSERTSTATEMENT
+    INSERT INTO albums (
+    label_code, artist, title, label, format, released, date_added
+    ) VALUES (
+    :label_code, :artist, :title, :label, :format, :released, :date_added
+    );
+    INSERTSTATEMENT
+
+    prepared_statement = @db.prepare(insert_statement)
+    #now that we have a prepared statement let's iterate the csv and use its values to populate our database
+
+    CSV.foreach(FILE_PATH, headers: true) do |row|#tells it that first line is header info, not data
+      prepared_statement.execute(row.to_h)  #knows because of headers
+    end
 
   end
+
 end
+
+
 
 release_db = ReleaseDatabase.new
 #potato = ReleaseDatabase.new   #this would connect to the same database
 release_db.reset_schema!
+release_db.load!
